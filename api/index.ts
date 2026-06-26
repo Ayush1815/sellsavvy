f9f099db24401b9698a668572698cd7670f64181
Ayush Kumar Giri <ayush77619@gmail.com>
2026-06-26 23:58:23 +0530

    debug: add crash wrapper

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,13 @@
-import app from '../server/index';
-
-export default app;
+export default async function (req, res) {
+  try {
+    const module = await import('../server/index.ts');
+    const app = module.default;
+    return app(req, res);
+  } catch (err) {
+    res.status(500).json({
+      error: "Vercel Crash Debug",
+      message: err.message,
+      stack: err.stack
+    });
+  }
+}
