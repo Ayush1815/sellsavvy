2375e1fb72b3a7d707107aef10e64c0d51500fb6
Ayush Kumar Giri <ayush77619@gmail.com>
2026-06-27 00:00:11 +0530

    fix: ESM import extension

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,2 @@
-export default async function (req, res) {
-  try {
-    const module = await import('../server/index.ts');
-    const app = module.default;
-    return app(req, res);
-  } catch (err) {
-    res.status(500).json({
-      error: "Vercel Crash Debug",
-      message: err.message,
-      stack: err.stack
-    });
-  }
-}
+import app from '../server/index.js';
+export default app;
